43c5ec8d5b114aa8211985ff5f8c28f1b1a20b4a
Pawel Baclawski <baclawski.pawel@gmail.com>
2016-12-12 11:47:59 +0100

    Added configuration files

--- csa_rest_client.rb
+++ csa_rest_client.rb
@@ -1,46 +1,50 @@
 require 'rest-client'
 require 'json'
+require 'yaml'
 
 class  CsaRestClient
     def run
         initialise
         menu_loop
+        save_config
     end  
 
     private
 
     def initialise
-        @remotes = ['http://localhost:3000/api/broadcasts.json', 'https://csa-heroku-pab37.herokuapp.com/api/broadcasts.json']
-        @api_url = @remotes[1] # default
+        read_config
+        
+        @api_url = @remotes[0] # default API
 
-        @login = "admin"
-        @password = "taliesin"
+        @login = ""
+        @password = ""
     end
 
     def menu_loop
         while true do
-            puts "\n\t\tCSA Rest Client"
+            puts "-" * @console_width
+            puts "CSA REST CLIENT".center(@console_width)
             puts
-            puts "Remote: #{@api_url}"
+            puts "Remote: #{@api_url}".center(@console_width)
             puts
-            puts "1. Login\n" +
-                 "2. List existing broadcasts\n" +
-                 "3. Create a new broadcast\n" +
-                 "4. Change remote\n" +
-                 "Q. Quit"
+            puts "1. List existing broadcasts\n" +
+                 "2. Create a new broadcast\n" +
+                 "3. Change remote server"
+            if @login != ""
+                puts "4. Logout"
+            end
+            puts "Q. Quit"
             
-            answ = gets.chomp
-            puts
-            case answ
+            case gets.chomp.upcase
             when "1"
-                login
-            when "2"
                 list_broadcasts
-            when "3"
+            when "2"
                 create_broadcast
-            when "4"
+            when "3"
                 change_remote
-            when "q", "Q"
+            when "4"
+                logout
+            when "Q"
                 break
             else
                 puts "Wrong answear, try again"
@@ -48,80 +52,187 @@ class  CsaRestClient
         end
     end
 
-    def login
-        puts
+    def logout
+        @login = ""
+        @password = ""
+        puts "Logged out"
     end
 
     def change_remote
+        puts "\nChose remote server:"
         @remotes.each_with_index do |remote, index|
             puts "#{index}. #{remote}"
         end
+        puts "\nN. Add new remote"
+        puts "Q. Back"
 
-        answ = gets.chomp
-        if @remotes.index(answ)
-            @api_url = @remotes[answ]
+        answ = gets.chomp.upcase
+        puts answ.ord
+        if answ.to_i != 0 || answ.ord == 48     # Check if integer or string as letter.to_i is always 0 and "0".ord == 48
+            @api_url = @remotes[answ.to_i]
+        elsif answ == "N"
+            print "URL: "
+            @remotes << @api_url = gets.chomp   # Set @api_url and add to @remotes
+        elsif answ == "Q"
+            return
         else
-            puts "\nWrong number, try again."
+            puts "\nWrong answear, try again."
         end
     end
 
     def list_broadcasts
-        begin
-            response = RestClient::Request.execute method: :get, url: @api_url,
-                            user: @login, password: @password
-            #puts "\nResponse code: #{response.code}\n\n"
-            
-            broadcasts = JSON.parse(response)
+        puts "BROADCASTS".center(@console_width)
+        puts
+
+        response = send_request(:get)
+
+        if !response then return end
+
+        broadcasts = JSON.parse(response)
+        if broadcasts.length > 0
             broadcasts.each do |broadcast|
-                broadcast.each do |item, value|
-                    puts "#{item} = #{value}"
+                puts "-" * @console_width
+
+                # User / Date
+                puts broadcast["created_at"].center(@console_width)
+                puts ("#{broadcast["user"]["firstname"]} " +
+                        "#{broadcast["user"]["surname"]} " +
+                        "(ID: #{broadcast["user"]["id"]})").center(@console_width)
+                puts "#{broadcast["url"]}".center(@console_width)
+                
+                # Feeds
+                @feeds = []
+                broadcast["feeds"].each do |f|
+                    @feeds << f["name"]
                 end
+                puts @feeds.join(', ').center(@console_width)
                 puts
+
+                # Message
+                puts reformat_wrapped(broadcast["content"])
+                puts                    
             end
-        rescue => exception
-            puts exception
+        else
+            puts "There are no existing broadcasts".center(@console_width)
         end
     end
 
     def create_broadcast
-        begin
-            message = "Lalalala"
-            #print "Message:"
-            #message.gets
-            #print "Shorten URL:" # FOR TWITTER ONLY TODO
-            #short_url.gets
-            #puts "Select feds:"
-            #print "Email [y]:"
-            #b_email.gets
-            #print "Facebook [y]:"
-            #b_facebook.gets
-            #print "RSS [y]:"
-            #b_rss.gets
-            #print "Atom [y]:"
-            #b_atom.gets
-            #print "Twitter [y]:"
-            #b_twitter.gets
-            request = 
-                {   
-                    broadcast: {content: message},
-                    #shorten_url: short_url,
-                    feeds: {
-                        email: "0",
-                        facebook: "0",
-                        RSS: "0",
-                        atom: "0",
-                        twitter: "0",
-                        instant_messenger: "1"
-                    }
-                }
+        request = Hash.new()
+        feeds = Hash.new()
 
-            response = RestClient::Request.execute method: :post, url: $api_url,
-                            user: $login, password: $password, payload: request
-            #puts response
+        print "Message: "
+        request[:broadcast] = {content: gets.chomp}
+        if request[:broadcast][:content] == ""
+            puts "Message cannot be empty, try again"
+            return
+        end
+
+        puts "Select feds:"
+        print "Email [n]: "
+        if gets.chomp.upcase == 'Y'
+            feeds[:email] = "1" 
+        end
+        print "Facebook [n]: "
+        if gets.chomp.upcase == 'Y'
+            feeds[:facebook] = "1"
+        end
+        print "RSS [n]: "
+        if gets.chomp.upcase == 'Y'
+            feeds[:RSS] = "1"
+        end
+        print "Atom [n]: "
+        if gets.chomp.upcase == 'Y'
+            feeds[:atom] = "1"
+        end
+        print "Twitter [n]: "
+        if gets.chomp.upcase == 'Y'
+            feeds[:twitter] = "1"
+
+            print "Shorten URL: "
+            request[:shorten_url] = gets.chomp
+        end
+        print "Notification feed [n]: "
+        if gets.chomp.upcase == 'Y'
+            feeds[:notification_feed] = "1"
+        end
+
+        if feeds.length != 0 # FIXME Why do I need to use != ?
+            request[:feeds] = feeds
+        else
+            puts "You did not chose any feeds, try again"
+            return
+        end
+
+        response = send_request(:post, request)
+    end
+
+    def send_request(method, request = {})
+        if @login == ""
+            puts "Please, enter your credentials:"
+            print "Login: "
+            @login = gets.chomp
+            print "Password: "
+            @password = gets.chomp
+        end
+
+        begin
+            RestClient::Request.execute method: method, url: @api_url,
+                        user: @login, password: @password, payload: request
+        rescue => exception
+            @login = ""
+            @password = ""
+
+            puts "\nWrong password or remote server down"
+            puts "Error: #{exception}"
+            puts
+        end
+        
+    end
+
+    def read_config
+        begin
+            config = YAML.load_file("config/config.yml")
+            @console_width = config["console_width"]
+            @remotes = config["remotes"]
+        rescue => exception
+            # If no config file found or file corrupted - set default values
+            @console_width = 60
+            @remotes = ['http://localhost:3000/api/broadcasts.json', 'https://csa-heroku-pab37.herokuapp.com/api/broadcasts.json']
+        end     
+    end
+
+    def save_config     # FIXME or delete
+        begin
+            config = YAML.load_file("config/config.yml")
+            config["console_width"] = @console_width
+            config["remotes"] = @remotes
+            File.open("config/config.yml", 'w') {|f| f.write config.to_yaml}
         rescue => exception
             puts exception
+            puts "Changes will not be saved - config file not found or corrupted"
         end
     end
+
+    # Ready solution to wrap too long broadcast's message
+    # https://www.safaribooksonline.com/library/view/ruby-cookbook/0596523696/ch01s15.html
+    def reformat_wrapped(s, width = @console_width)
+        lines = []
+	    line = ""
+        s.split(/\s+/).each do |word|
+            if line.size + word.size >= width
+                lines << line
+                line = word
+            elsif line.empty?
+                line = word
+            else
+                line << " " << word
+            end
+        end
+        lines << line if line
+        return lines.join "\n"
+	end
+
 end
 
 csa_rest_client = CsaRestClient.new
